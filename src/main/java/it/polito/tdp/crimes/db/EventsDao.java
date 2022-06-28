3372998d1cfbe56b7c615e792c4fa72e9fcb0a06
Daniele Sferlazzo <S272348@studenti.polito.it>
2022-06-28 18:45:50 +0200

    Progetto completato

--- src/main/java/it/polito/tdp/crimes/db/EventsDao.java
+++ src/main/java/it/polito/tdp/crimes/db/EventsDao.java
@@ -6,6 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+import it.polito.tdp.crimes.model.Adiacenza;
 import it.polito.tdp.crimes.model.Event;
 
 
@@ -49,6 +53,58 @@ public class EventsDao {
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	public List<String> getVertici(String categoria, int mese){
+		String sql = "SELECT DISTINCT offense_type_id "
+				+ "FROM events "
+				+ "WHERE offense_category_id=? AND MONTH(reported_date)=?";
+		List<String> result = new ArrayList<String>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				result.add(rs.getString("offense_type_id"));
+			}
+			conn.close();
+			return result;
+		}  catch (SQLException e) {
+			e.printStackTrace();
+			return null ;
+		}
+	}
+	
+	public List<Adiacenza> getArchi(String categoria, int mese){
+		String sql = "SELECT e1.offense_type_id AS id1, e2.offense_type_id AS id2, COUNT(DISTINCT e1.neighborhood_id) AS n "
+				+ "FROM EVENTS e1, EVENTS e2 "
+				+ "WHERE e1.neighborhood_id=e2.neighborhood_id AND e1.offense_type_id>e2.offense_type_id "
+				+ "	AND e1.offense_category_id=e2.offense_category_id AND e1.offense_category_id=? "
+				+ "	AND MONTH(e1.reported_date)=? AND MONTH(e1.reported_date)= MONTH(e2.reported_date) "
+				+ "GROUP BY e1.offense_type_id, e2.offense_type_id";
+		List<Adiacenza> result = new ArrayList<Adiacenza>();
+		
+		try {
+			Connection conn = DBConnect.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setString(1, categoria);
+			st.setInt(2, mese);
+			
+			ResultSet rs = st.executeQuery();
+			while(rs.next()) {
+				Adiacenza a = new Adiacenza(rs.getString("id1"), rs.getString("id2"), rs.getInt("n"));
+				result.add(a);
+			}
+			conn.close();
+			return result;
+			
+		} catch (SQLException e) {
 			e.printStackTrace();
 			return null ;
 		}
--- src/main/java/it/polito/tdp/crimes/model/Model.java
+++ src/main/java/it/polito/tdp/crimes/model/Model.java
@@ -1,6 +1,85 @@
 package it.polito.tdp.crimes.model;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.jgrapht.Graph;
+import org.jgrapht.Graphs;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+import it.polito.tdp.crimes.db.EventsDao;
 
 public class Model {
+	
+	private Graph<String, DefaultWeightedEdge> grafo;
+	private EventsDao dao;
+	
+	private List<String> best;
+	
+	public Model() {
+		dao = new EventsDao();
+	}
+	
+	public void creaGrafo(String categoria, int mese) {
+		grafo = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		//AGGIUNGO I VERTICI
+		Graphs.addAllVertices(grafo, dao.getVertici(categoria, mese));
+		
+		//AGGIUNGO GLI ARCHI
+		List<Adiacenza> archi = dao.getArchi(categoria, mese);
+		for(Adiacenza a : archi) {
+			Graphs.addEdgeWithVertices(grafo, a.getV1(), a.getV2(), a.getPeso());
+		}
+		
+		System.out.println("VERTICI: " + grafo.vertexSet().size()+"\nARCHI: " + grafo.edgeSet().size());
+	}
+	
+	public List<Adiacenza> getArchiMaggioriPM(){ 
+		double pesoTot = 0;
+		for(DefaultWeightedEdge e : grafo.edgeSet()) {
+			pesoTot+= this.grafo.getEdgeWeight(e); 
+		}
+		double avg = pesoTot/this.grafo.edgeSet().size();
+		
+		List<Adiacenza> result = new ArrayList<Adiacenza>();
+		for(DefaultWeightedEdge e : grafo.edgeSet()) {
+			if(this.grafo.getEdgeWeight(e)>avg)
+				result.add(new Adiacenza(this.grafo.getEdgeSource(e), this.grafo.getEdgeTarget(e),
+						(int) this.grafo.getEdgeWeight(e)));
+		}
+		return result;
+				
+	}
+	
+	public List<String> calcolaPercorso(String sorgente, String destinazione){
+		best = new LinkedList<>();
+		List<String> parziale = new LinkedList<>();
+		parziale.add(sorgente);
+		cerca(parziale, destinazione);
+		return best;
+	}
+	
+	private void cerca(List<String> parziale, String destinazione) {
+		
+		if(parziale.get(parziale.size()-1).equals(destinazione)) {	//CASO TERMINALE
+			//CONTROLLO SE E LA SOLUZIONE MIGLIORE
+			if(parziale.size()>best.size())
+				best= new LinkedList<>(parziale);
+			return;
+		}
+		
+		//SCORRO I VICINI DELL ULTIMO INSERITO E PROVO LE VARIE STRADE
+		for(String v : Graphs.neighborListOf(grafo, parziale.get(parziale.size()-1))) {
+			if(!parziale.contains(v)) {
+				parziale.add(v);
+				cerca(parziale, destinazione);
+				parziale.remove(parziale.size()-1);
+			}
+		}
+		
+	}
+	
 	
 }
